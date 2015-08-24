0156a18f663c5a7628d73aa9c0951975699f79f6
Rui Meireles <ruivitormeireles@gmail.com>
2015-08-24 16:11:14 +0100

    Bootstrap examples + Javascript + holder.js

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django import template
+from django.http import Http404
 
 def post_list(request):
     #posts = Post.objects.all()
@@ -16,4 +18,10 @@ def post_detail(request, pk):
     #post = Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+    
+def example(request, id):
+    try:
+        return render(request, 'blog/example' + id + '.html', {})
+    except template.TemplateDoesNotExist:
+        raise Http404
 
